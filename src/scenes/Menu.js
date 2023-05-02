a708eb04596ee2cb2cf0a5584a5a9573cf0e65d2
m-fausto-29 <129785531+m-fausto-29@users.noreply.github.com>
2023-05-02 15:47:13 -0700

    testing title

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -1,1 +1,43 @@
 //Menu scene
+class Menu extends Phaser.Scene(){
+    constructor(){
+        super("menuScene");
+    }
+    preload() {
+        this.load.image('title', './assets/title.png');   // load title screen
+    }
+
+    create(){
+
+        // add titlescreen
+        this.title = this.add.tileSprite(0, 0, game.config.width, game.config.height, 'title').setOrigin(0, 0);
+
+        // define keys
+        keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+        keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+    }
+
+    update() {
+        if(Phaser.Input.Keyboard.JustDown(keyLEFT)) {
+            // easy mode
+            game.settings = {
+                spaceshipSpeed: 3,
+                fastshipSpeed: 3 + 2,
+                gameTimer: 60000
+            }
+            this.sound.play('sfx_select');
+            this.scene.start('playScene');
+        }
+        if(Phaser.Input.Keyboard.JustDown(keyRIGHT)) {
+            // hard mode
+            game.settings = {
+                spaceshipSpeed: 4,
+                fastshipSpeed: 4 + 2,
+                gameTimer: 45000
+            }
+            this.sound.play('sfx_select');
+            this.scene.start('playScene');
+        }
+    }
+    
+}
