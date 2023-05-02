aa769baff0f7c8dafcf0e7ce2c3146457b85a290
m-fausto-29 <129785531+m-fausto-29@users.noreply.github.com>
2023-05-02 15:50:04 -0700

    continuing testing title

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -16,28 +16,5 @@ class Menu extends Phaser.Scene(){
         keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
         keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
     }
-
-    update() {
-        if(Phaser.Input.Keyboard.JustDown(keyLEFT)) {
-            // easy mode
-            game.settings = {
-                spaceshipSpeed: 3,
-                fastshipSpeed: 3 + 2,
-                gameTimer: 60000
-            }
-            this.sound.play('sfx_select');
-            this.scene.start('playScene');
-        }
-        if(Phaser.Input.Keyboard.JustDown(keyRIGHT)) {
-            // hard mode
-            game.settings = {
-                spaceshipSpeed: 4,
-                fastshipSpeed: 4 + 2,
-                gameTimer: 45000
-            }
-            this.sound.play('sfx_select');
-            this.scene.start('playScene');
-        }
-    }
     
 }
