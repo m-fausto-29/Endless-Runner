19ddea6a196304adaafd5d9ad929f683463e12ea
m-fausto-29 <129785531+m-fausto-29@users.noreply.github.com>
2023-05-02 15:35:23 -0700

    adding menu,play, index files

--- src/main.js
+++ src/main.js
@@ -1,1 +1,15 @@
 //main file
+let config = {
+    type: Phaser.AUTO,
+    width: 640,
+    height: 480,
+    scene: [ Menu, Play ] 
+}
+let game = new Phaser.Game(config);
+
+// reserve keyboard vars
+let keyUP, keyLEFT, keyRIGHT;
+
+//set UI sizes
+let borderUISize = game.config.height / 15;
+let borderPadding = borderUISize / 3;
