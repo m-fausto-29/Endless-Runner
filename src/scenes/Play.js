254d3dbfc92e95f70c11334941766ead729b7c6b
m-fausto-29 <129785531+m-fausto-29@users.noreply.github.com>
2023-05-07 21:47:48 -0700

    preloading current assets

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -1,1 +1,19 @@
-// restarting play.js file
+// Play.js
+let highScore = 0;
+let tries = 0;
+class Play extends Phaser.Scene{
+    constructor(){
+        super("Play");
+    }
+    preload(){
+        //load images/tile sprites
+        this.load.image('plain_road', 'assets/plain_road.png');
+        this.load.spritesheet('witch', 'assets/witch.png',{
+            frameWidth: 49,
+            frameHeight: 77,
+        });
+        this.load.image('pitchfork', 'assets/pitchfork.png');
+        this.load.image('tree', 'assets/tree.png');
+    }
+
+}
