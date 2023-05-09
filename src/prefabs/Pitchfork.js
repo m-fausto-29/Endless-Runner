54329865fde077c55195e3c2343678a6bfa6a579
m-fausto-29 <129785531+m-fausto-29@users.noreply.github.com>
2023-05-08 18:33:16 -0700

    implementing pitchfork class

--- src/prefabs/Pitchfork.js
+++ src/prefabs/Pitchfork.js
@@ -1,1 +1,20 @@
 //adding pitchfork class
+class Pitchfork extends Phaser.GameObjects.Sprite{
+    constructor(scene, x, y, texture, frame, multiplier){
+        super(scene, x, y, texture, frame);
+        this.worldScene = scene;
+        this.speed = speed;
+        this.multiplier = multiplier;
+        scene.physics.add.existing(this);
+        this.displayWidth = game.config.width * (2/15);
+        this.displayHeight = game.config.height * (4/15);
+    }
+
+    update(){
+        this.body.setVelocityY(this.worldScene.scrollSpeed * this.multiplier);
+        this.body.setVelocityX(this.speed * this.multiplier);
+        if(this.y > game.config.height + this.height) {
+            this.destroy();
+        }
+    }
+}
