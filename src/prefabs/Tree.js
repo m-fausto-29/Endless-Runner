adf0a6d4a2f5881e96a97c884a6bbe74d1bd8836
m-fausto-29 <129785531+m-fausto-29@users.noreply.github.com>
2023-05-08 18:27:02 -0700

    implementing tree class

--- src/prefabs/Tree.js
+++ src/prefabs/Tree.js
@@ -1,1 +1,17 @@
 // Tree class
+class Tree extends Phaser.GameObjects.Sprite{
+    constructor(scene, x, y, texture, frame) {
+        super(scene, x, y, texture, frame);
+        this.worldScene = scene;
+        scene.physics.add.existing(this);
+        this.displayWidth = game.config.width * (1/15);
+        this.displayHeight = game.config.height * (2/15);
+    }
+
+    update(){
+        this.body.setVelocityY(this.worldScene.scrollSpeed);
+        if(this.y > game.config.height + this.height) {
+            this.destroy();
+        }
+    }
+}
