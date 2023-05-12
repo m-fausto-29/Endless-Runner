c6783143f89b906cf625a2ced55bedd29eb77e65
m-fausto-29 <129785531+m-fausto-29@users.noreply.github.com>
2023-05-11 20:24:51 -0700

    adding texture atlas in tutorial page

--- src/scenes/Tutorial.js
+++ src/scenes/Tutorial.js
@@ -4,6 +4,7 @@ class Tutorial extends Phaser.Scene {
     }
     preload(){
         this.load.image('tutorial', './assets/tutorialpage1.png');
+        this.load.atlas('girl', 'assets/witch_atlas.png', 'assets/witch_atlas.json');
     }
 
     create(){
@@ -11,12 +12,31 @@ class Tutorial extends Phaser.Scene {
 
         // adding title screen
         this.tutorialPage = this.add.tileSprite(0, 0, game.config.width, game.config.height, 'tutorial').setOrigin(0, 0);
+        this.tutorialPage.setDepth(0);
+
+        this.anims.create({
+            key: 'float',
+            frames: this.anims.generateFrameNames('girl', {
+                prefix: 'float(',
+                start: 1,
+                end: 6,
+                suffix: ')'
+            }),
+            frameRate: 10,
+            repeat: -1      // loop animation
+        });
+
+        // add girl
+        this.girl = this.add.sprite(game.config.width/2, game.config.height/2.6, 'girl', 'float(1)');
+
 
         // defining keys
         keyENTER = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ENTER);
     }
 
     update(){
+        this.girl.play('float', true);
+        
         if(Phaser.Input.Keyboard.JustDown(keyENTER)){
             this.scene.start('Play');
         }
